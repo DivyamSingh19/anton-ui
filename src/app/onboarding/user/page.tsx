a3aa3ec63cf8810b13c46f01acbd3ade14d16d00
Divyam Singh <divyamsingh1918@gmail.com>
2026-03-13 23:47:58 +0530

    updates

--- src/app/onboarding/user/page.tsx
+++ src/app/onboarding/user/page.tsx
@@ -1,9 +1,11 @@
+"use client"
 import React from 'react'
+import CyberButton from '@/components/buttons/secondary'
 
 const page = () => {
   return (
     <div>
-      
+      <CyberButton label="Start Monitoring" onClick={() => console.log("monitoring started")} />
     </div>
   )
 }
